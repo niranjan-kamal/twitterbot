0497ffb82b0dc8525294a9206532925ef04dba56
niranjan-kamal <niranjank3007@gmail.com>
2025-05-02 15:55:52 -0400

    Add files via upload

---
+++ keepalive.py
@@ -0,0 +1,15 @@
+from flash import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Keepalive service is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
